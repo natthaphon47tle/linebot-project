e00826a67367c2b809e72e98b9a69bee10c5e112
Natthaphon <natthaphon47tle@gmail.com>
2026-05-20 13:18:30 +0700

    fix login page

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ def login():
 
                 session["user"] = username
 
-                return """
+return """
 <!DOCTYPE html>
 
 <html>
@@ -218,7 +218,7 @@ button{
     font-size:16px;
     cursor:pointer;
     width:100%;
-}ห
+}
 
 </style>
 
